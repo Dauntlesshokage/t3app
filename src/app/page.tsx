674a40711d180f991762a903a0f3819794564a5b
Ananth Lakshman Kumar <ananthhockage@gmail.com>
2024-06-21 15:25:40 -0400

    Database added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { db } from "~/server/db";
 import Link from "next/link";
+import { posts } from "~/server/db/schema";
 
 const mockUrls = [
   "https://utfs.io/f/12a2fc4e-4a9f-45e4-85a7-58123b6b84b1-yzd674.jpg",
@@ -11,12 +15,18 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
-export default function HomePage() {
+export default async function HomePage() {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="h-70 w-48 p-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="h-70 w-48 p-4">
             <img src={image.url} alt="image" />
           </div>
         ))}
