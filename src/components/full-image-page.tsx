4896d9da129051f9991692ae18786c52e3ad3d31
Ananth Lakshman Kumar <ananthhockage@gmail.com>
2024-06-23 20:40:16 -0400

    Modal styles

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,8 +4,12 @@ import { getImage } from "../server/queries";
 async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
   return (
-    <div className="flex h-full w-full bg-green-500">
-      <img src={image.url} className="w-96 object-contain" />;
+    <div className="flex h-full w-full min-w-0 ">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="flex-shrink object-contain" />
+      </div>
+      <div className=" flex w-48 flex-shrink flex-col border-l"></div>
+      <div className="text-xl font-bold">{image.name}</div>
     </div>
   );
 }
